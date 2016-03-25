0c66935f68cb190f7a1ca17b2e82d35914a618fc
AntonKueltz <kueltz.anton@gmail.com>
2016-03-24 17:17:07 -0700

    clean up and bug fixes

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ curvemath = Extension("fastecdsa.curvemath",
                       libraries=['gmp'],
                       sources=['src/curveMath.c', 'src/curve.c', 'src/point.c'])
 
-curvemath = Extension("fastecdsa._ecdsa",
-                      include_dirs=['src/'],
-                      libraries=['gmp'],
-                      sources=['src/_ecdsa.c', 'src/curveMath.c', 'src/curve.c', 'src/point.c'])
+_ecdsa = Extension("fastecdsa._ecdsa",
+                   include_dirs=['src/'],
+                   libraries=['gmp'],
+                   sources=['src/_ecdsa.c', 'src/curveMath.c', 'src/curve.c', 'src/point.c'])
 
 setup(name='fastecdsa',
       version='0.1',
       description='Fast elliptic curve arithmetic',
       packages=['fastecdsa'],
-      ext_modules=[curvemath])
+      ext_modules=[curvemath, _ecdsa])
