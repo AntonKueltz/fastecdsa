0f1cd764cb62db9b4dd18d7bbee4a1e98e1948f1
AntonKueltz <kueltz.anton@gmail.com>
2016-03-28 15:51:49 -0700

    flags for C compiler in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from distutils.core import setup, Extension
 
 
-curvemath = Extension('fastecdsa.curvemath',
-                      include_dirs=['src/'],
-                      libraries=['gmp'],
-                      sources=['src/curveMath.c', 'src/curve.c', 'src/point.c'])
+curvemath = Extension(
+    'fastecdsa.curvemath',
+    include_dirs=['src/'],
+    libraries=['gmp'],
+    sources=['src/curveMath.c', 'src/curve.c', 'src/point.c'],
+    extra_compile_args=['-std=c99', '-O2']
+)
 
-_ecdsa = Extension('fastecdsa._ecdsa',
-                   include_dirs=['src/'],
-                   libraries=['gmp'],
-                   sources=['src/_ecdsa.c', 'src/curveMath.c', 'src/curve.c', 'src/point.c'])
+_ecdsa = Extension(
+    'fastecdsa._ecdsa',
+    include_dirs=['src/'],
+    libraries=['gmp'],
+    sources=['src/_ecdsa.c', 'src/curveMath.c', 'src/curve.c', 'src/point.c'],
+    extra_compile_args=['-std=c99', '-O2']
+)
 
 setup(
     name='fastecdsa',
