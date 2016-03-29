085c4d331617594342e79f7ecd484e69d65b14b8
AntonKueltz <kueltz.anton@gmail.com>
2016-03-28 17:40:05 -0700

    pep8 compliance

--- fastecdsa/ecdsa.py
+++ fastecdsa/ecdsa.py
@@ -8,13 +8,13 @@ from Crypto.Random.random import randint  # Use python secure random for now
 
 class KeyPair:
     def __init__(self, curve):
-        self.d = randint(2, curve.q-1)
+        self.d = randint(2, curve.q - 1)
         self.Q = curve.pointMul(curve.G, self.d)
         self.curve = curve
 
     def sign(self, msg):
         hashed = sha256(msg).hexdigest()
-        k = randint(2, self.curve.q-1)
+        k = randint(2, self.curve.q - 1)
         r, s = _ecdsa.sign(hashed, str(self.d), str(k), self.curve.name)
         return int(r), int(s)
 
