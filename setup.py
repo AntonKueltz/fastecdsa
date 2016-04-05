39f649f21251077e4f40c40ab952b83168b7b66a
AntonKueltz <kueltz.anton@gmail.com>
2016-04-05 15:48:35 -0700

    v1.0.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ _ecdsa = Extension(
 
 setup(
     name='fastecdsa',
-    version='1.0.2',
+    version='1.0.3',
     author='Anton Kueltz',
     author_email='kueltz.anton@gmail.com',
     license='CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
