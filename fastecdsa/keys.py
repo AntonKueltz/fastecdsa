f725f3d640bc7c6a9c5b3b5f6599b3b7bfdb93de
AntonKueltz <kueltz.anton@gmail.com>
2018-01-10 20:54:30 -0800

    add a generic export function

--- fastecdsa/keys.py
+++ fastecdsa/keys.py
@@ -70,3 +70,31 @@ def get_public_key(d, curve):
         fastecdsa.point.Point: The public key, a point on the given curve.``
     """
     return d * curve.G
+
+
+def export_key(key, curve=None, filepath=None):
+    """Export a public or private ket in PEM format.
+
+    Args:
+        |   key (fastecdsa.point.Point | long): A public or private EC key
+        |   curve (fastecdsa.curve.Curve): The curve corresponding to the key (required if the
+            key is a private key)
+        |   filepath (str): Where to save the exported key. If None the key is simply printed.
+    """
+    from .point import Point
+    from .asn1 import encode_keypair, encode_public_key
+
+    if isinstance(key, Point):
+        encoded = encode_public_key(key)
+    elif curve is None:
+        raise ValueError('curve paramter cannot be \'None\' when exporting a private key')
+    else:
+        pubkey = key * curve.G
+        encoded = encode_keypair(key, pubkey)
+
+    if filepath is None:
+        print(encoded)
+    else:
+        f = open(filepath, 'w')
+        f.write(encoded)
+        f.close()
