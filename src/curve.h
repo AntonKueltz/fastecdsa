ddd07ed6e4bf5f5bd6c1c2f3726d4166b5f57d6a
AntonKueltz <kueltz.anton@gmail.com>
2018-03-10 10:51:01 -0800

    remove curve specific constructors

--- src/curve.h
+++ src/curve.h
@@ -14,12 +14,4 @@ typedef struct {
 CurveZZ_p * buildCurveZZ_p(char * p, char * a, char * b, char * q, char * gx, char * gy, int base);
 void destroyCurveZZ_p(CurveZZ_p * curve);
 
-CurveZZ_p * buildP192(void);
-CurveZZ_p * buildP224(void);
-CurveZZ_p * buildP256(void);
-CurveZZ_p * buildP384(void);
-CurveZZ_p * buildP521(void);
-
-CurveZZ_p * buildSecp256k1(void);
-
 #endif
