f74b883d2cc596341c9e66bf1a23113d710bdc23
AntonKueltz <kueltz.anton@gmail.com>
2018-04-30 21:10:47 -0700

    mock out the C extension modules in the sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,14 @@
 from datetime import datetime
 import os
 import sys
+from unittest import mock
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+
+MOCK_MODULES = ['fastecdsa._ecdsa', 'fastecdsa.curvemath']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 
 # -- General configuration ------------------------------------------------
