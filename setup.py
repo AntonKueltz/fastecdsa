e48d0441f888f9931a1e02cdf5e0b8902b45bac1
AntonKueltz <kueltz.anton@gmail.com>
2019-02-14 21:03:54 -0800

    promote to v1.7.1 (oops)

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ _ecdsa = Extension(
 
 setup(
     name='fastecdsa',
-    version='1.7.0',
+    version='1.7.1',
     author='Anton Kueltz',
     author_email='kueltz.anton@gmail.com',
     license='CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
