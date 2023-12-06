3258e0d02fde441b8eea4fdff8dbfffcd28bb95d
Anton Kueltz <kueltz.anton@gmail.com>
2023-12-05 23:46:22 -0800

    bundle test vectors as part of project

--- fastecdsa/tests/test_rfc6979_ecdsa.py
+++ fastecdsa/tests/test_rfc6979_ecdsa.py
@@ -1,130 +1,72 @@
+from dataclasses import dataclass
 from hashlib import sha1, sha224, sha256, sha384, sha512
-from re import findall, DOTALL
-from urllib.request import urlopen, Request
+from json import load
+from typing import Callable, List
 from unittest import TestCase
 
 from ..curve import P192, P224, P256, P384, P521
 from ..ecdsa import sign
 from ..util import RFC6979
 
+hash_lookup = {
+    '1': sha1,
+    '224': sha224,
+    '256': sha256,
+    '384': sha384,
+    '512': sha512
+}
+
+
+@dataclass
+class TestVector:
+    h: Callable[bytes, bytes]
+    m: str
+    k: int
+    r: int
+    s: int
+    
+    def __init__(self, data: dict):
+        self.h = hash_lookup[data["h"]]
+        self.m = data["m"]
+        self.k = data["k"]
+        self.r = data["r"]
+        self.s = data["s"]
+
+
+@dataclass
+class TestData:
+    q: int
+    x: int
+    vectors: List[TestVector]
+        
+    def __init__(self, data: dict):
+        self.q = data["q"]
+        self.x = data["x"]
+        self.vectors = [TestVector(d) for d in data["test_case"]]
+
 
 class TestRFC6979ECDSA(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.rfc6979_text = urlopen(Request('https://tools.ietf.org/rfc/rfc6979.txt', headers={'User-Agent': 'python'})).read().decode()
-        cls.hash_lookup = {
-            '1': sha1,
-            '224': sha224,
-            '256': sha256,
-            '384': sha384,
-            '512': sha512
-        }
+    def _run_test(self, filename, curve):
+        with open(filename) as f:
+            data = load(f)
+        
+        test_data = TestData(data)
+        
+        for test in test_data.vectors:
+            self.assertEqual(test.k, RFC6979(test.m, test_data.x, test_data.q, test.h).gen_nonce())
+            self.assertEqual((test.r, test.s), sign(test.m, test_data.x, curve=curve, hashfunc=test.h))
 
     def test_p192_rfc6979_ecdsa(self):
-        curve_tests = findall(r'curve: NIST P-192(.*)curve: NIST P-224', self.rfc6979_text, flags=DOTALL)[0]
-
-        q = int(findall(r'q = ([0-9A-F]*)', curve_tests)[0], 16)
-        x = int(findall(r'x = ([0-9A-F]*)', curve_tests)[0], 16)
-
-        test_regex = r'With SHA-(\d+), message = "([a-zA-Z]*)":\n' \
-                     r'\s*k = ([0-9A-F]*)\n' \
-                     r'\s*r = ([0-9A-F]*)\n' \
-                     r'\s*s = ([0-9A-F]*)\n'
-
-        for test in findall(test_regex, curve_tests):
-            h = self.hash_lookup[test[0]]
-            msg = test[1]
-            k = int(test[2], 16)
-            r = int(test[3], 16)
-            s = int(test[4], 16)
-
-            self.assertEqual(k, RFC6979(msg, x, q, h).gen_nonce())
-            self.assertEqual((r, s), sign(msg, x, curve=P192, hashfunc=h))
+        self._run_test("fastecdsa/tests/vectors/rfc6979/P192.json", P192)
 
     def test_p224_rfc6979_ecdsa(self):
-        curve_tests = findall(r'curve: NIST P-224(.*)curve: NIST P-256', self.rfc6979_text, flags=DOTALL)[0]
-
-        q = int(findall(r'q = ([0-9A-F]*)', curve_tests)[0], 16)
-        x = int(findall(r'x = ([0-9A-F]*)', curve_tests)[0], 16)
-
-        test_regex = r'With SHA-(\d+), message = "([a-zA-Z]*)":\n' \
-                     r'\s*k = ([0-9A-F]*)\n' \
-                     r'\s*r = ([0-9A-F]*)\n' \
-                     r'\s*s = ([0-9A-F]*)\n'
-
-        for test in findall(test_regex, curve_tests):
-            h = self.hash_lookup[test[0]]
-            msg = test[1]
-            k = int(test[2], 16)
-            r = int(test[3], 16)
-            s = int(test[4], 16)
-
-            self.assertEqual(k, RFC6979(msg, x, q, h).gen_nonce())
-            self.assertEqual((r, s), sign(msg, x, curve=P224, hashfunc=h))
+        self._run_test("fastecdsa/tests/vectors/rfc6979/P224.json", P224)
 
     def test_p256_rfc6979_ecdsa(self):
-        curve_tests = findall(r'curve: NIST P-256(.*)curve: NIST P-384', self.rfc6979_text, flags=DOTALL)[0]
-
-        q = int(findall(r'q = ([0-9A-F]*)', curve_tests)[0], 16)
-        x = int(findall(r'x = ([0-9A-F]*)', curve_tests)[0], 16)
-
-        test_regex = r'With SHA-(\d+), message = "([a-zA-Z]*)":\n' \
-                     r'\s*k = ([0-9A-F]*)\n' \
-                     r'\s*r = ([0-9A-F]*)\n' \
-                     r'\s*s = ([0-9A-F]*)\n'
-
-        for test in findall(test_regex, curve_tests):
-            h = self.hash_lookup[test[0]]
-            msg = test[1]
-            k = int(test[2], 16)
-            r = int(test[3], 16)
-            s = int(test[4], 16)
-
-            self.assertEqual(k, RFC6979(msg, x, q, h).gen_nonce())
-            self.assertEqual((r, s), sign(msg, x, curve=P256, hashfunc=h))
+        self._run_test("fastecdsa/tests/vectors/rfc6979/P256.json", P256)
 
     def test_p384_rfc6979_ecdsa(self):
-        curve_tests = findall(r'curve: NIST P-384(.*)curve: NIST P-521', self.rfc6979_text, flags=DOTALL)[0]
-
-        q_parts = findall(r'q = ([0-9A-F]*)\n\s*([0-9A-F]*)', curve_tests)[0]
-        q = int(q_parts[0] + q_parts[1], 16)
-        x_parts = findall(r'x = ([0-9A-F]*)\n\s*([0-9A-F]*)', curve_tests)[0]
-        x = int(x_parts[0] + x_parts[1], 16)
-
-        test_regex = r'With SHA-(\d+), message = "([a-zA-Z]*)":\n' \
-                     r'\s*k = ([0-9A-F]*)\n\s*([0-9A-F]*)\n' \
-                     r'\s*r = ([0-9A-F]*)\n\s*([0-9A-F]*)\n' \
-                     r'\s*s = ([0-9A-F]*)\n\s*([0-9A-F]*)\n'
-
-        for test in findall(test_regex, curve_tests):
-            h = self.hash_lookup[test[0]]
-            msg = test[1]
-            k = int(test[2] + test[3], 16)
-            r = int(test[4] + test[5], 16)
-            s = int(test[6] + test[7], 16)
-
-            self.assertEqual(k, RFC6979(msg, x, q, h).gen_nonce())
-            self.assertEqual((r, s), sign(msg, x, curve=P384, hashfunc=h))
+        self._run_test("fastecdsa/tests/vectors/rfc6979/P384.json", P384)
 
     def test_p521_rfc6979_ecdsa(self):
-        curve_tests = findall(r'curve: NIST P-521(.*)curve: NIST K-163', self.rfc6979_text, flags=DOTALL)[0]
-
-        q_parts = findall(r'q = ([0-9A-F]*)\n\s*([0-9A-F]*)\n\s*([0-9A-F]*)', curve_tests)[0]
-        q = int(q_parts[0] + q_parts[1] + q_parts[2], 16)
-        x_parts = findall(r'x = ([0-9A-F]*)\n\s*([0-9A-F]*)\n\s*([0-9A-F]*)', curve_tests)[0]
-        x = int(x_parts[0] + x_parts[1] + x_parts[2], 16)
-
-        test_regex = r'With SHA-(\d+), message = "([a-zA-Z]*)":\n' \
-                     r'\s*k = ([0-9A-F]*)\n\s*([0-9A-F]*)\n\s*([0-9A-F]*)\n' \
-                     r'\s*r = ([0-9A-F]*)\n\s*([0-9A-F]*)\n\s*([0-9A-F]*)\n' \
-                     r'\s*s = ([0-9A-F]*)\n\s*([0-9A-F]*)\n\s*([0-9A-F]*)\n'
-
-        for test in findall(test_regex, curve_tests):
-            h = self.hash_lookup[test[0]]
-            msg = test[1]
-            k = int(test[2] + test[3] + test[4], 16)
-            r = int(test[5] + test[6] + test[7], 16)
-            s = int(test[8] + test[9] + test[10], 16)
-
-            self.assertEqual(k, RFC6979(msg, x, q, h).gen_nonce())
-            self.assertEqual((r, s), sign(msg, x, curve=P521, hashfunc=h))
+        self._run_test("fastecdsa/tests/vectors/rfc6979/P521.json", P521)
--- fastecdsa/tests/test_whycheproof_vectors.py
+++ fastecdsa/tests/test_whycheproof_vectors.py
@@ -1,9 +1,7 @@
 from binascii import unhexlify
-from json import JSONDecodeError, loads
-from sys import version_info
-from unittest import SkipTest, TestCase, skipIf
-from urllib.error import URLError
-from urllib.request import urlopen, Request
+from hashlib import sha224, sha256, sha384, sha3_224, sha3_256, sha3_384, sha3_512, sha512
+from json import load
+from unittest import TestCase, skipIf
 
 from fastecdsa.curve import (
     P224, P256, P384, P521,
@@ -14,22 +12,14 @@ from fastecdsa.ecdsa import verify
 from fastecdsa.encoding.der import DEREncoder
 from fastecdsa.encoding.sec1 import SEC1Encoder
 
-try:
-    from hashlib import sha224, sha256, sha384, sha3_224, sha3_256, sha3_384, sha3_512, sha512
-except ImportError:
-    pass
 
-
-@skipIf(version_info.minor < 6, "Test requires sha3 (added in python3.6) to run")
 class TestWycheproofEcdsaVerify(TestCase):
     @staticmethod
-    def _get_tests(url):
-        try:
-            test_raw = urlopen(Request(url, headers={'User-Agent': 'python'})).read()
-            test_json = loads(test_raw)
-            return test_json["testGroups"]
-        except (JSONDecodeError, URLError) as error:
-            SkipTest("Skipping tests, could not download / parse data from {}\nError: {}".format(url, error))
+    def _get_tests(filename):
+        with open(filename) as f:
+            test_json = load(f)
+        
+        return test_json["testGroups"]
 
     def _test_runner(self, tests, curve, hashfunc):
         for test_group in tests:
@@ -49,126 +39,126 @@ class TestWycheproofEcdsaVerify(TestCase):
                     self.assertFalse(test["result"] == "valid", test)
 
     def test_brainpool224r1_sha224(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_brainpoolP224r1_sha224_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_brainpoolP224r1_sha224_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, brainpoolP224r1, sha224)
 
     def test_brainpoolP256r1_sha256(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_brainpoolP256r1_sha256_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_brainpoolP256r1_sha256_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, brainpoolP256r1, sha256)
 
     def test_brainpoolP320r1_sha384(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_brainpoolP320r1_sha384_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_brainpoolP320r1_sha384_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, brainpoolP320r1, sha384)
 
     def test_brainpoolP384r1_sha384(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_brainpoolP384r1_sha384_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_brainpoolP384r1_sha384_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, brainpoolP384r1, sha384)
 
     def test_brainpoolP512r1_sha512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_brainpoolP512r1_sha512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_brainpoolP512r1_sha512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, brainpoolP512r1, sha512)
 
     def test_p224_sha224(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp224r1_sha224_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp224r1_sha224_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P224, sha224)
 
     def test_p224_sha256(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp224r1_sha256_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp224r1_sha256_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P224, sha256)
 
     def test_p224_sha3_224(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp224r1_sha3_224_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp224r1_sha3_224_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P224, sha3_224)
 
     def test_p224_sha3_256(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp224r1_sha3_256_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp224r1_sha3_256_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P224, sha3_256)
 
     def test_p224_sha3_512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp224r1_sha3_512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp224r1_sha3_512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P224, sha3_512)
 
     def test_p224_sha512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp224r1_sha512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp224r1_sha512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P224, sha512)
 
     def test_secp256k1_sha256(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256k1_sha256_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256k1_sha256_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, secp256k1, sha256)
 
     def test_secp256k1_sha3_256(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256k1_sha3_256_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256k1_sha3_256_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, secp256k1, sha3_256)
 
     def test_secp256k1_sha3_512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256k1_sha3_512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256k1_sha3_512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, secp256k1, sha3_512)
 
     def test_secp256k1_sha512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256k1_sha512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256k1_sha512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, secp256k1, sha512)
 
     def test_p256_sha256(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256r1_sha256_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256r1_sha256_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P256, sha256)
 
     def test_p256_sha3_256(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256r1_sha3_256_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256r1_sha3_256_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P256, sha3_256)
 
     def test_p256_sha3_512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256r1_sha3_512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256r1_sha3_512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P256, sha3_512)
 
     def test_p256_sha512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp256r1_sha512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp256r1_sha512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P256, sha512)
 
     def test_p384_sha384(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp384r1_sha384_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp384r1_sha384_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P384, sha384)
 
     def test_p384_sha3_384(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp384r1_sha3_384_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp384r1_sha3_384_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P384, sha3_384)
 
     def test_p384_sha3_512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp384r1_sha3_512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp384r1_sha3_512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P384, sha3_512)
 
     def test_p384_sha512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp384r1_sha512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp384r1_sha512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P384, sha512)
 
     def test_p521_sha3_512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp521r1_sha3_512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp521r1_sha3_512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P521, sha3_512)
 
     def test_p521_sha512(self):
-        url = "https://raw.githubusercontent.com/google/wycheproof/master/testvectors/ecdsa_secp521r1_sha512_test.json"
-        tests = self._get_tests(url)
+        filename = "fastecdsa/tests/vectors/whycheproof/ecdsa_secp521r1_sha512_test.json"
+        tests = self._get_tests(filename)
         self._test_runner(tests, P521, sha512)
